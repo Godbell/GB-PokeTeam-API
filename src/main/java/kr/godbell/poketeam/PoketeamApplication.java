e851e79c7cbddc10957ee4412f2e6034202c8fa3
Godbell <12191579@inha.edu>
2023-03-29 15:14:45 +0900

    Initial Commit

---
+++ src/main/java/kr/godbell/poketeam/PoketeamApplication.java
@@ -0,0 +1,13 @@
+package kr.godbell.poketeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/godbell/poketeam/PoketeamApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.godbell.poketeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoketeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
