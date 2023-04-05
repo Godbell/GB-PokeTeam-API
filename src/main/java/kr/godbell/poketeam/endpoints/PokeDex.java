b6282b440070c9371babd22bf2a13330d7f772d8
Godbell <12191579@inha.edu>
2023-04-05 18:47:11 +0900

    add test endpoint and model /api/v1/pokedex/pokemon/

--- src/main/java/kr/godbell/poketeam/endpoints/PokeDex.java
+++ src/main/java/kr/godbell/poketeam/endpoints/PokeDex.java
@@ -2,12 +2,29 @@ package kr.godbell.poketeam.endpoints;
 
 import org.springframework.web.bind.annotation.*;
 
+import kr.godbell.poketeam.models.Pokemon;
+
 @RestController
 public class PokeDex
 {
-    @GetMapping("/api/v1/pokedex")
+	@GetMapping("/api/v1/pokedex")
 	public String pokeDex()
-    {
+	{
 		return "PokéDex";
+	}
+
+	@GetMapping("api/v1/pokedex/pokemon/{idOrName}")
+	@ResponseBody
+	public Pokemon pokemon(
+			@PathVariable("idOrName") String idOrName,
+			@RequestParam(defaultValue = "en") String lang
+		)
+	{
+		Pokemon pokemon = new Pokemon();
+
+		pokemon.setId(0);
+		pokemon.setName("TEST");
+
+		return pokemon;
 	}
 }
---
+++ src/main/java/kr/godbell/poketeam/models/Pokemon.java
@@ -0,0 +1,12 @@
+package kr.godbell.poketeam.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Pokemon
+{
+    public int id;
+    public String name;
+}
