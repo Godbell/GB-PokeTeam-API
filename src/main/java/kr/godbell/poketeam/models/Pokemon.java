e64401e94d94fe06c0176dfc0ad27c69cf04e583
Godbell <12191579@inha.edu>
2023-04-08 14:27:06 +0900

    remove unused dependency

--- src/main/java/kr/godbell/poketeam/models/Pokemon.java
+++ src/main/java/kr/godbell/poketeam/models/Pokemon.java
@@ -2,8 +2,6 @@ package kr.godbell.poketeam.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
